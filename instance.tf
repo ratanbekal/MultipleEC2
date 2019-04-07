eecae4fde9f3369603be012f83b20dedd3901e11
shruthiWadageri <wadageri@gmail.com>
2019-04-07 06:46:26 +0000

    Updated

--- instance.tf
+++ instance.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "web" {
+  count = "2"
   ami = "ami-013e04dfa544cfe54"
   instance_type = "t2.micro"
   key_name = "MyKeyPair"
   tags {
-    Name = "Terraformsinglejenkins"
+    Name = "Terraform-${count.index + 1}"
   }
 
 }
