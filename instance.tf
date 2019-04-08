05b248931696d2ce9a85039a544945d0cd6ec8aa
shruthiWadageri <wadageri@gmail.com>
2019-04-08 23:16:31 +0530

    Updated

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   count = "2"
   ami = "ami-013e04dfa544cfe54"
   instance_type = "t2.micro"
-  key_name = "MyKeyPair"
+  key_name = "Clientvisit2019"
   security_groups = ["${var.security_g}"]
   tags {
     Name = "Terraform-${count.index + 1}"
