28977ed53188612e507832a621b8d20d900fcd66
shruthiWadageri <wadageri@gmail.com>
2019-04-11 13:17:04 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "web" {
   count = "3"
-  ami = "ami-0481727d0e48e4da7"
+  ami = "ami-0edbcc3ba422f19d4"
   instance_type = "t2.micro"
   key_name = "${var.keypair}"
   security_groups = ["${var.security_g}"]
   subnet_id = "${var.subnet_id}"
   tags {
-    Name = "Terraform-${count.index + 1}"
+    Name = "Seleniumnode-${count.index + 1}"
   }
   provisioner "local-exec" {
     inline = [
