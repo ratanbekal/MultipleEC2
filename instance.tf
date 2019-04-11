a23010cea76b88c0798d2921f516a6a54088106e
shruthiWadageri <wadageri@gmail.com>
2019-04-11 23:04:13 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "web" {
-  count = "1"
+  count = "3"
   ami = "ami-0edbcc3ba422f19d4"
   instance_type = "t2.micro"
   key_name = "${var.keypair}"
   security_groups = ["${var.security_g}"]
   subnet_id = "${var.subnet_id}"
   tags {
-    Name = "Seleniumhubnode-${count.index + 1}"
+    Name = "hubnode-${count.index + 1}"
   }
 }
